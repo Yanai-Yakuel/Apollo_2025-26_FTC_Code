d48404d34fc8df0bae037b25cb3491902376ca36
Apollo9662 <apollo9662@gmail.com>
2026-01-28 16:28:44 +0200

    Update hoodtest.java

--- TeamCode/hoodtest.java
+++ TeamCode/hoodtest.java
@@ -8,7 +8,7 @@ public class ServoTest extends OpMode {
     Servo hood;
     boolean flag = true;
     boolean flag2 = true;
-    double pos = 1;
+    double pos = 0.1;
 
     @Override
     public void init() {
@@ -18,11 +18,73 @@ public class ServoTest extends OpMode {
     @Override
     public void loop() {
         if (gamepad1.a && flag) {
-            pos += 1;
+            pos += 0.1;
             flag = false;
         } else if (!gamepad1.a && !flag) flag = true;
         if (gamepad1.b && flag2) {
-            pos += 1;
+            pos += 0.1;
+            flag2 = false;
+        } else if (!gamepad1.b && !flag2) flag2 = true;
+
+        pos = Math.min(1.0, pos);
+        hood.setPosition(pos);
+    }
+}package teleops;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class ServoTest extends OpMode {
+
+    Servo hood;
+    boolean flag = true;
+    boolean flag2 = true;
+    double pos = 0.1;
+
+    @Override
+    public void init() {
+        hood = hardwareMap.get(Servo.class, "s_hood");
+    }
+
+    @Override
+    public void loop() {
+        if (gamepad1.a && flag) {
+            pos += 0.1;
+            flag = false;
+        } else if (!gamepad1.a && !flag) flag = true;
+        if (gamepad1.b && flag2) {
+            pos += 0.1;
+            flag2 = false;
+        } else if (!gamepad1.b && !flag2) flag2 = true;
+
+        pos = Math.min(1.0, pos);
+        hood.setPosition(pos);
+    }
+}package teleops;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class ServoTest extends OpMode {
+
+    Servo hood;
+    boolean flag = true;
+    boolean flag2 = true;
+    double pos = 0.1;
+
+    @Override
+    public void init() {
+        hood = hardwareMap.get(Servo.class, "s_hood");
+    }
+
+    @Override
+    public void loop() {
+        if (gamepad1.a && flag) {
+            pos += 0.1;
+            flag = false;
+        } else if (!gamepad1.a && !flag) flag = true;
+        if (gamepad1.b && flag2) {
+            pos += 0.1;
             flag2 = false;
         } else if (!gamepad1.b && !flag2) flag2 = true;
 
